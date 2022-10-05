0a628c7d66b264fc0a5309d8e569e579214ca470
Matthew Miller <millem@amazon.com>
2022-10-05 12:46:39 -0700

    separateThreadPoolCaller-new

--- test/sdk-benchmarks/src/main/java/software/amazon/awssdk/benchmark/NettyImprovementsBenchmark.java
+++ test/sdk-benchmarks/src/main/java/software/amazon/awssdk/benchmark/NettyImprovementsBenchmark.java
@@ -70,7 +70,7 @@ public class NettyImprovementsBenchmark {
     }
 
     public static void main(String... args) throws Exception {
-        separateEventLoopCaller();
+        separateThreadPoolCaller();
     }
 
     public static void separateEventLoopCaller() throws Exception {
